63d850ef817873b1641c1c600108f47994f0e73e
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-05-22 15:30:22 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -2,7 +2,7 @@ function solution(hp) {
   let answer = 0;
   let antArmy = [5, 3, 1];
   for (let ant of antArmy) {
-    if (hp === 0) break;
+    //if (hp === 0) break;
     answer += Math.floor(hp / ant);
     hp = hp % ant;
   }

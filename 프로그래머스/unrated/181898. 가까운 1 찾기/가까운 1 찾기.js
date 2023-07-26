2d318f712c9fafc2523a60f74a219eedc5531de7
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-07-26 20:38:17 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,8 @@
+function solution(arr, idx) {
+    for (let i = 0; i < arr.length; i++) {
+        if (i >= idx && arr[i] === 1) {
+            return i;
+        } else continue;
+    }
+    return -1;
+}

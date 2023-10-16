c08f7fd8568f99bad639a36863a721c9af7dda32
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-10-16 20:00:55 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,8 @@
+function solution(seoul) {
+    let locationKim = seoul.indexOf('Kim');
+    return `김서방은 ${locationKim}에 있다`;
+}
+
+
+// Kim은 오직 한 번만, 잘못된 값이 입력되는 경우는 없다.
+// string `김서방은 {x}에 있다.`

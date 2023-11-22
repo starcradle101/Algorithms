e8de6cdddd776e3e18d49b2e8b50059ae1e3663e
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-22 20:07:21 +0900

    [Bronze V] Title: A+B - 4, Time: 160 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+let input = [];
+
+rl.on("line", function(line){
+    input.push(line.split(' ').map(el => +el));
+}).on("close", function(){
+    for(let i = 0; i < input.length; i++) {
+        console.log(input[i][0] + input[i][1]);
+    }
+    process.exit();
+})

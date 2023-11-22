a96cc36ccb18c316012d35935cd797a81f5a2235
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-23 08:23:42 +0900

    [Bronze V] Title: 문자와 문자열, Time: 136 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,18 @@
+function solution(input) {
+  console.log(input[0].charAt(Number(input[1]) - 1));
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});

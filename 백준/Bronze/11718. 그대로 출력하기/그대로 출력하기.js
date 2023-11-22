3fac819393c852e9ae7edec4acd3d48ba6a1c89a
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-23 08:25:18 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 184 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,19 @@
+function solution(input) {
+  const answer = input.join('\n');
+  console.log(answer);
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});

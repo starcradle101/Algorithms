55aa6156dec964e1e106fc90432dbfb6e40646e4
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-27 23:50:48 +0900

    [Bronze III] Title: 최소, 최대, Time: 576 ms, Memory: 115612 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,24 @@
+function solution(input) {
+  const arrMax = Math.max(...input[1]);
+  const arrMin = Math.min(...input[1]);
+  console.log(`${arrMin} ${arrMax}`);
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let list = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  input.forEach(val => {
+    list.push(val.split(' ').map(el => parseInt(el)));
+  });
+  solution(list);
+  process.exit();
+});

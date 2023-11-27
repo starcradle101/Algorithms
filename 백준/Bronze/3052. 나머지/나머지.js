8c587dac20a55cf847528a525747b85236fbdf5d
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-27 23:48:51 +0900

    [Bronze II] Title: 나머지, Time: 144 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,24 @@
+function solution(input) {
+  let flattenInput = input.flatMap(num => num % 42);
+  const answer = new Set(flattenInput);
+  console.log(answer.size);
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let list = [];
+
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  input.forEach(val => {
+    list.push(val.split(' ').map(el => parseInt(el)));
+  });
+  solution(list);
+  process.exit();
+});

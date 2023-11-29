d50ebdda673a7fefa6b95896df142d3ffc60de60
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-11-29 15:54:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 152 ms, Memory: 9704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,20 @@
+function solution(input) {
+  console.log(input - 543);
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on('line', function (line) {
+  input = line;
+  input = parseInt(input);
+  rl.close();
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});

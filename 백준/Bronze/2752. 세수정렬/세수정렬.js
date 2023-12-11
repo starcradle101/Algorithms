33dfe2569da81df069347a5d6ed7e84adf55431f
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-12-11 14:25:10 +0900

    [Bronze IV] Title: 세수정렬, Time: 148 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,22 @@
+function solution(input) {
+  console.log(input.sort((a, b) => a - b).join(' '));
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+let list = [];
+
+rl.on('line', function (line) {
+  input = line;
+  rl.close();
+}).on('close', function () {
+  list = input.split(' ').map(el => Number(el));
+
+  solution(list);
+  process.exit();
+});

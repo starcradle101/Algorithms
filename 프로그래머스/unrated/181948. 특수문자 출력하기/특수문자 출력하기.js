3b75607059858295ee42347825ece3ca8174073c
Hoon Kang <113353436+starcradle101@users.noreply.github.com>
2023-12-13 10:44:08 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 42.37 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    let specialCharacters = '!@#$%^&*(\\\'"<>?:;';
+console.log(specialCharacters);
+
+});

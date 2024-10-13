ee2fc36b608e55f46ffd3698c0dd73f1a2612dfe
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2024-10-13 19:09:18 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,15 @@
+function solution(a, b, c) {
+    let answer = 0;
+    if (a === b && b === c) {
+        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3);
+    } 
+
+    else if (a === b || a === c || b === c) {
+        return (a + b + c) * (a**2 + b**2 + c**2);
+    } 
+
+    else {
+        return a + b + c;
+    }
+    return answer;
+}

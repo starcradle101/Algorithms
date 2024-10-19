6e4053ac0ebcd35622d623a592f3eca2b25ffcbf
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2024-10-19 16:02:58 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, is_suffix) {
-    if (my_string.length < is_suffix) return 0;
-    
-    const suffix_length = is_suffix.length;
-    for (let i = 0; i < my_string.length; i += 1) {
-        if (my_string.slice(i) === is_suffix) return 1;
-    }
-    
-    return 0;
+    return +my_string.endsWith(is_suffix);
 }

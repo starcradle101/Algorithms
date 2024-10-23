7838be233a8dad4a8472ea42dd577cba951bbf53
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2024-10-23 22:52:50 +0900

    [level 0] Title: 글자 지우기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,8 +1,11 @@
 function solution(my_string, indices) {
     let answer = '';
-    const str_array = my_string.split('');
-    for (let i = 0; i < str_array.length; i += 1) {
-      if (!indices.includes(i)) answer += str_array[i];
+    for(let i = 0; i < my_string.length; i += 1) {
+        if (!indices.includes(i)) {
+            answer += my_string[i];
+        } else {
+            continue;
+        }
     }
     return answer;
 }

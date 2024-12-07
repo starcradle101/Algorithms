f1a11f7942000154c625cdec36517c004bb434d9
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2024-12-07 22:08:06 +0900

    [level 0] Title: 배열의 유사도, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+function solution(s1, s2) {
+    let answer = 0;
+    for (let i = 0; i < s1.length; i += 1) {
+        for (let j = 0; j < s2.length; j += 1) {
+            if (s1[i] === s2[j]) answer += 1;
+        }
+    }
+    return answer;
+}

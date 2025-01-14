6d8b6b71495d92f1ba642261d9f6e6adc3469c65
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-14 19:26:15 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,16 +1,7 @@
 function solution(n) {
-    let divisorArray = [];
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            divisorArray.push(i);
-        } else {
-            continue;
-        }
+    let answer = 0;
+    for (let i = 1; i <= n; i += 1) {
+        if (n % i === 0) answer += i;
     }
-    let answer = divisorArray.reduce((acc, curr) => acc + curr, 0);
     return answer;
 }
-
-// 정수 n의 약수의 합을 구하는 문제
-// 빈 배열을 만들고, 1부터 n까지 n을 나누어 떨어지게 하는 수들만을 저장
-// 저장된 배열을 reduce를 통해서 합을 구하고 반환

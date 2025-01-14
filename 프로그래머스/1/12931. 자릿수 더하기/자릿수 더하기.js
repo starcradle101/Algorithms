0aaaae479ffb3dcfa7e67168bf507e9be9dcfbde
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-14 19:27:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,4 @@
 function solution(n)
 {
-    var answer = 0;
-    return String(n).split('').reduce((acc, curr) => acc + Number(curr),0);
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    return String(n).split('').reduce((acc, curr) => acc + Number(curr) ,0);
 }
-
-// 문자열로 바꾸고 split 한 다음 reduce 한 결과를 보내기

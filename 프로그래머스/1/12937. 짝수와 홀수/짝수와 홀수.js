cc6fcf26fe351e6d7cac59c77d94b4066a08d68f
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-15 18:46:07 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,3 @@
 function solution(num) {
-    if (num === 0) {
-        return "Even";
-    }
-    return (num % 2 === 1 || num % 2 === -1) ? "Odd" : "Even";
+    return num % 2 === 0 ? 'Even' : 'Odd';
 }
-
-// 짝수일 경우 Even 홀수인 경우 Odd
-//

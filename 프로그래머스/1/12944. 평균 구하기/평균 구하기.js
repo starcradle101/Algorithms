41b6064161bf933e924f36a517d1061dd3183fe6
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-15 18:47:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,1 @@
-function solution(arr) {
-    let answer = arr.reduce((acc, curr) => acc + curr) / arr.length;
-    return answer;
-}
-
-// arr의 평균을 return 하는 함수, solution
-// 배열을 전부 더해서 평균을 내는 것 -> reduce를 사용하자
+const solution = (arr) => arr.reduce((acc, curr) => acc + curr, 0) / arr.length;

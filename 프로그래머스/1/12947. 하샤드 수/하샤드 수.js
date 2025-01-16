188183f8fc9fc70308cb95736887314efc59e6e0
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-16 17:29:42 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,9 +1,5 @@
 function solution(x) {
-    let digitSum = (""+x).split('').reduce((acc, curr) => acc + parseInt(curr), 0);
-    if (x % digitSum === 0) {
-        return true;
-    } else return false;
+    const sumOfDigits = x.toString().split('').reduce((acc, curr) => acc + Number(curr), 0);
     
-    // harshad라면 x의 자릿수의 합으로 x가 나누어 떨어져야 한다.
-    // x % 자릿수의 합 === 0이라면 true, 아니면 false 반환
+    return x % sumOfDigits === 0;
 }

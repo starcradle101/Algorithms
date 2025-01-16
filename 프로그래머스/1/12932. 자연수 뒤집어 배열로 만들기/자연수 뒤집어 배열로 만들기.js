97345ee271295b150c71e2960000af145e3dd2d4
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-16 17:23:22 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,7 +1,3 @@
 function solution(n) {
-    return String(n).split('').reverse().map((x) => Number(x));
+    return n.toString().split('').reverse().map(el => Number(el));
 }
-
-// 어떤 자연수
-// 뒤집어서 배열형태로 리턴해야 함
-// 문자열로 만들고, split 한 다음, reverse 하고 배열을 

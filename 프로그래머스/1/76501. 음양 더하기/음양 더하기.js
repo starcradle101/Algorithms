744db3de34246dcff335e3edd269867a0e5a0d00
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-17 19:30:35 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,7 @@
 function solution(absolutes, signs) {
     let answer = 0;
-    for (let i = 0; i < signs.length; i++) {
-        (signs[i] === true) ? answer += absolutes[i] : answer -= absolutes[i];
+    for (let i = 0; i < absolutes.length; i += 1) {
+        answer += (signs[i] ? 1 : -1) * absolutes[i];
     }
     return answer;
 }
-
-// 배열을 돌면서 signs[i] 가 양수라면 answer에 +absolutes[i]

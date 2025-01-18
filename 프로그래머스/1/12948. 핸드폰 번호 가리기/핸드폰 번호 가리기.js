4ae32c247f4d0f42bb9cf2ab54958ced388eaba5
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-18 19:12:25 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,3 @@
 function solution(phone_number) {
-    let phone_number_array = [...phone_number];
-    for (let i = 0; i < phone_number_array.length - 4; i++) {
-        phone_number_array[i] = '*';
-    }
-    return phone_number_array.join('');
-
+    return phone_number.replace(/.(?=.{4})/g, '*');
 }
-// 배열로 받아와서 
-// 마지막 네 번째 요소까지를 제외한 요소를 변경 후 join

f645576781492e101b46b71d5a307549b6c2492f
Hoonie Hoon <113353436+starcradle101@users.noreply.github.com>
2025-01-21 15:08:22 +0900

    [level 1] Title: 직사각형 별찍기, Time: 53.77 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,9 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    for (let i = 0; i < b; i += 1) {
+        const line = '*'.repeat(a);
+        console.log(line);
+    }
+});

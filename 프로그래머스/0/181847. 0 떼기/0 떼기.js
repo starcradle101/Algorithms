61b963136858b7493e36832e19f2db6f9e34e35c
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-07 23:17:30 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,11 +1,7 @@
 function solution(n_str) {
-    let start_idx = 0;
-    for (let i = 0; i < n_str.length; i += 1) {
-        if (Number(n_str[i]) !== 0) {
-            start_idx += i;
-            break;
-        }
-    }
+    if(n_str[0] !== '0') return n_str;
     
-    return n_str.slice(start_idx);
+    for (let i = 0; i < n_str.length; i += 1) {
+        if(n_str[i] !== '0') return n_str.slice(i);
+    }
 }

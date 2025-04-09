a72b57c6014691f3d762a9e4044f8d3943f105fe
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-10 00:59:29 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,21 +1,15 @@
 function solution(num_list) {
-    let [sum_odd, sum_even] = [0, 0];
-    for (let i = 0; i < num_list.length; i += 1) {
-        if (i % 2 === 0) {
-            sum_odd += num_list[i];
-        } else {
-            sum_even += num_list[i];
-        }
+    let [odd_sum, even_sum] = [0, 0];
+    
+    for(let i = 0; i < num_list.length; i += 1) {
+        i % 2 === 0 ? odd_sum += num_list[i] : even_sum += num_list[i];
     }
     
-    if (sum_odd === sum_even) {
-        return sum_odd
-    } else {
-        return (sum_odd > sum_even) ? sum_odd : sum_even;
-    }
+    if(odd_sum === even_sum) return odd_sum;
+    
+    return odd_sum > even_sum ? odd_sum : even_sum;
 }
 
-// 가장 처음 1번 원소
-// 홀수 번째 원소들의 합
-// 짝수 번째 원소들의 합
-// 두 값이 같다면 둘 중 아무나, 아니라면 큰 값
+// 배열의 홀수/짝수 번째 원소들의 합 구하기
+// 두 합을 비교하기
+// 같다면 (우선 조건) 아무 값이나 return 하기

585bc29c5f4e159a2a8382f218b67a331666e60e
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-10 16:15:25 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,17 +1,6 @@
 function solution(date1, date2) {
-    const date1_num = Number(date1.join(''));
-    const date2_num = Number(date2.join(''));
+    const [date1_full, date2_full] = [date1.join(''), date2.join('')];
     
-    return (date1_num < date2_num) ? 1 : 0;
+    return +date1_full < +date2_full ? 1 : 0;
 }
 
-// date1 > date2 ? 1 : 0
-
-// 연도 비교
-
-// 월 비교
-
-// 일 비교
-
-// 크면 클수록 뒤로 간다 (앞서지 않는 날짜)
-// 앞서는 날짜 -> 비교했을때 작다!

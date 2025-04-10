735ee8108348aae03464d060190a6596535762a2
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-04-10 15:00:33 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,16 +1,22 @@
 function solution(myString) {
-    return myString.split('').map((el) => {
-        if (el === 'a') {
-            return 'A'
+    let answer = ''
+    for (let char of myString) {
+        if (char === 'a') {
+            answer += char.toUpperCase();
+        } else {
+            if (char.toUpperCase() === char && char !== 'A') {
+                answer += char.toLowerCase();
+            } else {
+                answer += char;
+            }    
         }
-        
-        if (el === el.toUpperCase() && el !== 'A') {
-            return el.toLowerCase();
-        }
-        
-        return el
-    }).join('')
+    }
+    return answer;
 }
 
-// a가 등장하면 전부 A
-// A 가아닌 대문자 알파벳은 모두 소문자로
+// step1. 문자열 순회하기
+// step2. 문자열이 a일 경우 A로 변환하기
+// step3. a가 아닌 문자일 경우
+// step3-1. 대문자라면 소문자 알파벳으로 변환하기
+// step3-2. 소문자라면 그대로 두기
+// 제한사항에서 주어지는 문자열은 전부 알파벳으로 이루어졌다고 했으니 다른 케이스 고려할 필요 없음

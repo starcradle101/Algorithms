38c2754fbee37a7fda043ae2b85cd0f30da6f293
starcradle101 <113353436+starcradle101@users.noreply.github.com>
2025-09-13 11:12:29 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
@@ -1,5 +1,3 @@
 class Solution {
-    fun solution(n: Int): Int {
-        return if(n % 7 != 0 ) n / 7 + 1 else n / 7
-    }
+    fun solution(n: Int): Int = if (n % 7 != 0) n / 7 + 1 else n / 7
 }
